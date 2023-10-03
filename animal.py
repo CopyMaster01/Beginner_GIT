91eaa68489e3b6b1e285beb8cdca47dc9f8beb75
CopyMaster01 <22050518@hdu.edu.cn>
2023-10-03 20:13:15 +0800

    Animal

--- animal.py
+++ animal.py
@@ -15,13 +15,10 @@ def main():
 
     if sys.argv[1] == 'cat':
         cat()
+    elif sys.argv[1] == 'dog':
+	dog()
     else:
-        default()
-
-	if sys.argv[1] == 'dog':
-		dog()
-	else:
-		default()
+	default()
 
 
 if __name__ == '__main__':
