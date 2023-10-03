d21f62d7944126443135c0d53d59ca3e26c902fa
CopyMaster01 <22050518@hdu.edu.cn>
2023-10-03 20:15:49 +0800

    Last

--- animal.py
+++ animal.py
@@ -12,13 +12,12 @@ def default():
     print('Hello')
 
 def main():
-
-    if sys.argv[1] == 'cat':
-        cat()
-    elif sys.argv[1] == 'dog':
-	dog()
-    else:
-	default()
+	if sys.argv[1] == 'cat':
+		cat()
+	elif sys.argv[1] == 'dog':
+		dog()
+	else:
+		default()
 
 
 if __name__ == '__main__':
